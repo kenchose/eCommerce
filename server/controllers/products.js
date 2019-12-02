a1e7775219aaa12a286f5a81b7ace5eefd596995
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2019-12-01 17:14:24 -0800

    product logic to getAll, onlyOne, and getCategories

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,99 +1,117 @@
-const Product = require('./../models/Product');
+const Product = require("./../models/Product");
 
 module.exports = {
-  create: async (req, res, next) => {
-    const {
-      imagePath,
-      name,
-      price,
-      description,
-      category
-    } = req.body;
+  // create: async (req, res, next) => {
+  //   console.log('product', req.body)
+  //   const {
+  //     imagePath,
+  //     brand,
+  //     name,
+  //     price,
+  //     description,
+  //     category
+  //   } = req.body;
 
-    //check if product already exist (by name)
-    const productExist = await Product.findOne({
-      "name": name
-    });
-    if (productExist) return res.status(400).send("Product already in your inventory");
+  //   //check if product already exist (by name)
+  //   const productExist = await Product.findOne({
+  //     "name": name
+  //   });
+  //   if (productExist) return res.status(400).send("Product already in your inventory");
 
-    try {
-      //if new product
-      const newProduct = new Product({
-        imagePath,
-        name,
-        price,
-        description,
-        category
-      });
-      const product = await newProduct.save();
-      res.status(200).json({
-        product
-      })
-    } catch (error) {
-      res.status(400).json(error);
-    }
-  },
+  //   try {
+  //     //if new product
+  //     const newProduct = new Product(req.body);
+  //     const product = await newProduct.save();
+  //     res.status(200).json({
+  //       newProduct: product
+  //     })
+  //   } catch (error) {
+  //     res.status(400).json({
+  //       success:false,
+  //       errorMessages: error
+  //     });
+  //   }
+  // },
 
   getAll: async (req, res, next) => {
     const products = await Product.find({});
     try {
-      if (!products) return res.status(400).json({
-        success: false,
-        message: "Cannot find products"
-      });
-      if (products) return res.status(200).json({
-        products: products
-      });
+      if (!products)
+        return res.status(400).json({
+          success: false,
+          errorMessage: "Cannot find products"
+        });
+      if (products)
+        return res.status(200).json({
+          products: products
+        });
     } catch (error) {
-      res.status(400).send(error)
+      res.status(400).json({
+        errorMessage: error
+      });
     }
   },
 
   onlyOne: async (req, res, next) => {
-    const id = req.params.id
+    const id = req.params.id;
     const product = await Product.findById({
       _id: id
     });
     try {
-      if (!product) return res.status(400).json({
-        success: false,
-        message: "No product found"
-      });
-      if (product) return res.status(200).json({
-        product
-      });
+      if (!product)
+        return res.status(400).json({
+          success: false,
+          errorMessage: "No product found"
+        });
+      if (product)
+        return res.status(200).json({
+          product
+        });
     } catch (error) {
       res.status(400).send(error);
-    };
+    }
   },
 
-  editProduct: async (req, res, next) => {
-    const id = req.params._id
-    const {
-      category,
-      name,
-      pricce
-    } = Product.findById({
-      _id: id
+  getCategory: async (req, res, next) => {
+    let category = req.params.category;
+    const products = await Product.find({
+      category: category
     });
-  },
-
-  addToCart: async (req, res, next) => {
-    const productId = req.params.id;
-    const product = await Product.findById({
-      _id: productId
-    });
-    if (!product) return res.status(400).send("No product found");
     try {
-      let cart = new Cart(req.session.cart ? req.session.cart : {});
-      cart.add(product, product.id)
-      req.session.cart = cart;
-      res.json({
-        cart
-      })
+      if (!products)
+        return res.status(400).json({
+          success: false,
+          errorMessage: `No ${category} category found`
+        });
+      if (products)
+        return res.status(200).json({
+          success: true,
+          message: `Found ${category} category`,
+          products: products
+        });
     } catch (error) {
-      res.status(400).send(error);
-    };
+      res.status(400).json({
+        errorMessage: error
+      });
+    }
   }
 
-}
+  // editProduct: async (req, res, next) => {
+  //   const id = req.params._id
+  //   const updateOps = {};
+  //   for (const ops of req.body) { //loop through req.body
+  //     updateOps[ops.propName] = ops.value //therefore we can change one or multiple key value pairs
+  //   }
+  //   Product.update({
+  //     _id: id
+  //   }, {
+  //     $set: updateOps
+  //   }, (err, product) => {
+  //     if (err) {
+  //       res.json(err)
+  //     } else {
+  //       res.json(product)
+  //     }
+  //   })
+  // },
+};
