849e97f92b63aae5262cc4a05d0201e1ce1de73b
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2019-12-20 20:35:29 -0800

    find store locations with google maps api

--- src/app/components/findstore/findstore.component.ts
+++ src/app/components/findstore/findstore.component.ts
@@ -1,19 +1,24 @@
 import {} from "googlemaps";
-import { Component, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnInit
+} from "@angular/core";
 
 @Component({
   selector: "app-findstore",
   templateUrl: "./findstore.component.html",
   styleUrls: ["./findstore.component.scss"]
 })
-export class FindstoreComponent implements AfterViewInit {
+export class FindstoreComponent implements AfterViewInit, OnInit {
   @ViewChild("map", { static: false }) mapElement: ElementRef;
   map: google.maps.Map;
-  // latitude = 34.0522;
-  // longitude = -118.2437;
-  // locationChosen: boolean = false;
 
   constructor() {}
+
+  ngOnInit() {}
 
   ngAfterViewInit() {
     // Map Options
@@ -57,27 +62,27 @@ export class FindstoreComponent implements AfterViewInit {
         coords: { lat: 34.0407, lng: -118.2468 },
         icon: "http://maps.google.com/mapfiles/kml/pal2/icon2.png",
         content:
-          "<h4>Headquarters</h4><p>611 E 7th St, Los Angeles, CA 90021, USA</p>"
+          "<h5 class='text-center'>Headquarters</h5><p>611 E 7th St, Los Angeles, CA 90021, USA</p>"
       },
       {
         coords: { lat: 34.0162, lng: -118.4968 },
         content:
-          "<h4>Third Street Promenade</h4><p>611 E 7th St, Los Angeles, CA 90021</p>"
+          "<h5 class='text-center'>Third Street Promenade</h5><p>611 E 7th St, Los Angeles, CA 90021</p>"
       },
       {
         coords: { lat: 34.0913, lng: -118.3794 },
         content:
-          "<h4>The Sunset Strip</h4><p>9040 W Sunset Blvd, West Hollywood, CA 90069</p>"
+          "<h5 class='text-center'>The Sunset Strip</h5><p>9040 W Sunset Blvd, West Hollywood, CA 90069</p>"
       },
       {
         coords: { lat: 34.0523, lng: -118.2395 },
         content:
-          "<h4>Little Tokyo Mall</h4><p>319 E 2nd St Los Angeles, CA 90012</p>"
+          "<h5 class='text-center'>Little Tokyo Mall</h5><p>319 E 2nd St Los Angeles, CA 90012</p>"
       },
       {
         coords: { lat: 34.0832, lng: -118.3588 },
         content:
-          "<h4>Melrose Avenue</h4><p>Melrose Ave., Los Angeles, CA 90046</p>"
+          "<h5 class='text-center'>Melrose Avenue</h5><p>Melrose Ave., Los Angeles, CA 90046</p>"
       }
     ];
 
@@ -88,8 +93,3 @@ export class FindstoreComponent implements AfterViewInit {
     });
   }
 }
-// onChoseLocation(location: any): void {
-//   this.latitude = location.coords.lat;
-//   this.longitude = location.coords.lng;
-//   // this.locationChosen = true;
-// }
